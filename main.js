b8a0eaf3a570e586bcae79cb06fe4d63bb5a22a0
chenjiangshuo <chenjiangshuo@netpas.co>
2019-05-06 12:46:53 +0800

    intercom 中点击邮箱用浏览器打开

--- main.js
+++ main.js
@@ -558,7 +558,7 @@ function windowCreateFinish(json) {
         client.write(json.targetID, 'new-window', {
             url: url
         })
-        if (url.indexOf('http://')>-1 || url.indexOf('https://')>-1){
+        if (url.indexOf('http://')>-1 || url.indexOf('https://')>-1 || url.indexOf('mailto')>-1){
             shell.openExternal(url)
             event.preventDefault();
         }
